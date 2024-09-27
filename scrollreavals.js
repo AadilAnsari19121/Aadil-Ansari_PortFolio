4a7ee5ed531b1dad92adf005ee8d40df503d6710
Aadil Ansari <115773388+AadilAnsari19121@users.noreply.github.com>
2024-09-27 19:32:16 +0530

    scroll animations

---
+++ scrollreavals.js
@@ -0,0 +1,21 @@
+ScrollReveal({
+    reset: false
+    // distance: '60px',
+    // duration: 2500,
+    // delay: 400
+
+});
+ScrollReveal().reveal('#navbar', { distance: '60px', duration: 1000, easing: 'ease', delay: 400, origin: 'top' });
+ScrollReveal().reveal('.about_me,.pr_img', { distance: '100px', duration: 2500, easing: 'ease', delay: 450, origin: 'left' });
+ScrollReveal().reveal('.my_contacts_logo img', { distance: '160px', duration: 1500, easing: 'ease', delay: 500, origin: 'right', interval: 200 });
+ScrollReveal().reveal('.my_img img', { distance: '100px', duration: 2500, easing: 'ease', delay: 600, origin: 'right' });
+ScrollReveal().reveal('.about_me_summery,.maps', { distance: '200px', duration: 2000, easing: 'ease', delay: 600, origin: 'left' });
+ScrollReveal().reveal('.section h3', { distance: '60px', duration: 1000, easing: 'ease', delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.skills_summery p', { distance: '60px', duration: 1200, easing: 'ease', delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.skills_name_logo .skills', { distance: '0px', duration: 1500, easing: 'ease', delay: 600, origin: 'right', interval: 200, scale: 0.5 });
+ScrollReveal().reveal('.experience,.ed1,.pr_sum h4', { distance: '160px', duration: 1500, easing: 'ease', delay: 500, origin: 'right' });
+ScrollReveal().reveal('.experience p,.ex_point li,.ed1 p,.ed1 ul li', { distance: '160px', duration: 1500, easing: 'ease', delay: 400, origin: 'right', interval: 150 });
+ScrollReveal().reveal('.pr_sumery li', { distance: '160px', duration: 1500, easing: 'ease', delay: 500, origin: 'right', interval: 200 });
+ScrollReveal().reveal('.pr_link img', { distance: '120px', duration: 1500, easing: 'ease', delay: 500, origin: 'right', rotate: { x: 0, y: 0, z: 180 } });
+ScrollReveal().reveal('.used_skills span', { distance: '260px', duration: 1500, easing: 'ease', delay: 700, origin: 'right', interval: 200, rotate: { x: 0, y: 180, z: 0 } });
+ScrollReveal().reveal('.contacts img,.contacts p', { distance: '260px', duration: 1000, easing: 'ease', delay: 400, origin: 'right', interval: 100 });
